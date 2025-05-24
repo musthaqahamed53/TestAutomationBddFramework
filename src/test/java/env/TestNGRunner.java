323bd95a26fcdaecd7577660cbda5f1248a6d003
Sheik Musthaq Ahamed <musthaq53ahamed@gmail.com>
2025-05-24 22:31:23 +0530

    initial_v5_jenkins

--- src/test/java/env/TestNGRunner.java
+++ src/test/java/env/TestNGRunner.java
@@ -4,11 +4,18 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 @CucumberOptions(dryRun = false, features = {"src/test/resources/features/addToCart.feature", "src/test/resources/features/searchProduct.feature"},
         glue = {"stepDefinitions", "env"}, monochrome = true, tags = "@SmokeTest or @Regex or @SearchTest or @AddToCart",
         plugin = {"pretty", "html:target/cucumber.html", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "rerun:target/failed_scenarios.txt"})
 public class TestNGRunner extends AbstractTestNGCucumberTests {
+
+
     @DataProvider(parallel = false)
     @Override
     public Object[][] scenarios() {
